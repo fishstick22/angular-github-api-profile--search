9cfab1838245f9018ffe966c3e268b2df9896ba4
Jeff <jeff.burkholder@gmail.com>
2017-06-18 14:39:01 -0500

    externalize credentials

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -5,12 +5,25 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService{
     private username:string;
-    private client_id = 'a2da278a6378b7341d79';
-    private client_secret = '15244353d4c7e25093f5993f46d3ff6aced7bc46';
+    private client_id:string;// = 'a2da278a6378b7341d79';
+    private client_secret:string;// = '15244353d4c7e25093f5993f46d3ff6aced7bc46';
     
     constructor(private _http: Http){
-        console.log('Github Service Ready...');
-        this.username = 'fishstick22';
+        console.log('Initializeing Github Service...');
+        this.getSecrets().subscribe(secrets => {
+            console.log(secrets);
+            this.username = secrets.username;
+            this.client_id = secrets.client_id;
+            this.client_secret = secrets.client_secret;
+        });
+
+        console.log('Github Service Ready');
+    }
+
+    getSecrets() {
+        return this
+          ._http.request('../github.secrets.json')
+          .map(res => res.json());
     }
     
     getUser(){
